bb4ff93d616be0cbf3738640286146672b6be17e
ChaurasiaP <pranshu1431@gmail.com>
2023-01-09 21:05:58 +0530

    added SECOND program

---
+++ Prog2.java
@@ -0,0 +1,11 @@
+package HundredPrograms;
+
+public class Prog2 {
+    public static void main(String[] args) {
+
+        int x=6;
+        int y=8;
+        System.out.println("Print Integers");
+        System.out.println(x+" "+y);
+    }
+}
