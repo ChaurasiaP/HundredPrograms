d70fed1f6161206823c28096c30b0071a7e88699
ChaurasiaP <pranshu1431@gmail.com>
2023-01-09 22:02:22 +0530

    added THIRD program

---
+++ Prog3.java
@@ -0,0 +1,11 @@
+package HundredPrograms;
+
+public class Prog3 {
+    public static void main(String[] args) {
+        System.out.println("Take command line argument");
+        for(String str:args){
+            System.out.print(str+" ");
+        }
+        System.out.println("Pranshu");
+    }
+}
